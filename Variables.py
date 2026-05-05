dbcf9f77d43ae2161e2d0ff8c118dbb0a50d6c7f
Swarnava1 <swarnavabose@gmail.com>
2026-05-05 20:00:12 +0200

    Rewrite

--- Variables.py
+++ Variables.py
@@ -1,14 +1,5 @@
 username="Bose"
 print(username)
 print("Hello")
-print(type(username))
-print("New world")
 
-print("New Text")
-
-print("New Text")
-print("New Text")
-print("New Text")
-
-print ("Bose")
 
